55e1827c4e84638c754a801871dc7d668229caf7
Josue Rivas <reycoc2311@gmail.com>
2022-06-15 13:15:18 -0600

    server file, done

--- app/models/Server.model.js
+++ app/models/Server.model.js
@@ -1,3 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('../db/config');
+
 class Server {
-  async connectToDB() {}
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    this.paths = {
+      events: '/api/events',
+    };
+
+    // Connect to db
+    this.connectToDB();
+
+    // Middlewares
+    this.middlewares();
+
+    // my app routes
+    this.routes();
+  }
+
+  async connectToDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    // CORS
+    this.app.use(cors());
+
+    // read and parse the body
+    this.app.use(express.json());
+
+    // public directory
+    this.app.use(express.static('public'));
+  }
+
+  routes() {
+    this.app.use(
+      this.paths.events,
+      require('../routes/events.route')
+    );
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(
+        '/** 🚀 Listening in port ',
+        this.port,
+        ' **/'
+      );
+    });
+  }
 }
+
+module.exports = Server;
