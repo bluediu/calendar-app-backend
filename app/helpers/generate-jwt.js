ddf24b24fe8336b264f56290686248570d24647f
Josue Rivas <reycoc2311@gmail.com>
2022-06-15 18:44:05 -0600

    docs

--- app/helpers/generate-jwt.js
+++ app/helpers/generate-jwt.js
@@ -1,5 +1,12 @@
 const jwt = require('jsonwebtoken');
 
+/**
+ * It takes a name and an optional uid, creates a payload, signs the payload with a secret key, and
+ * returns a promise that resolves with a token
+ * @param {name: string} - the name of the user
+ * @param {uid: string} - The user ID of the user who is logging in.
+ * @returns A promise that resolves to a token.
+ */
 const generateJWT = (name, uid = '') => {
   return new Promise((resolve, reject) => {
     const payload = { uid, name };
