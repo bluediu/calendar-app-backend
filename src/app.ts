3fc00c6c1ce75a704fd59ca670ead98162f7bbb8
jrivas <josuerivas2311@gmail.com>
2024-08-16 10:54:57 -0600

    Define app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Hello');
+import { envs } from './config';
+
+import { MongoDatabase } from './data/mongodb';
+
+import { AppRoutes } from './presentation/routes';
+
+import { Server } from './presentation/server';
+
+(() => {
+  main();
+})();
+
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_CNN,
+    dbName: envs.MONGO_DB_NAME,
+  });
+
+  new Server({ port: envs.PORT, routes: AppRoutes.routes }).start();
+}
